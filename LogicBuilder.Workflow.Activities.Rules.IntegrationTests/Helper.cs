85cd25512b8ef7a0a64996bccaf76e35a15081ff
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-02-05 14:30:39 -0500

    AB#17 Existing tests working - pushing to remote.

---
+++ LogicBuilder.Workflow.Activities.Rules.IntegrationTests/Helper.cs
@@ -0,0 +1,44 @@
+﻿using LogicBuilder.Workflow.Activities.Rules.IntegrationTests.Properties;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace LogicBuilder.Workflow.Activities.Rules.IntegrationTests
+{
+    internal static class Helper
+    {
+        internal static RuleValidation GetValidation(RuleSet ruleSet, Type type)
+        {
+            if (ruleSet == null)
+                throw new InvalidOperationException(Resources.ruleSetCannotBeNull);
+
+            RuleValidation ruleValidation = new(type);
+
+            if (!ruleSet.Validate(ruleValidation))
+            {
+                throw new InvalidOperationException
+                (
+                    string.Join
+                    (
+                        Environment.NewLine,
+                        ruleValidation.Errors.Aggregate
+                        (
+                            new List<string>
+                            {
+                                string.Format(CultureInfo.CurrentCulture, Resources.invalidRuleSetFormat, ruleSet.Name)
+                            },
+                            (list, next) =>
+                            {
+                                list.Add(next.ErrorText);
+                                return list;
+                            }
+                        )
+                    )
+                );
+            }
+
+            return ruleValidation;
+        }
+    }
+}
---
+++ LogicBuilder.Workflow.Activities.Rules.IntegrationTests/ParameterValidationTests.cs
@@ -0,0 +1,474 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicBuilder.Workflow.Activities.Rules.IntegrationTests
+{
+    public class ParameterValidationTests
+    {
+        [Fact]
+        public void Test_Rule_method_invocation_missing_required_parameter_throws_InvalidOperationException()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+
+            //Act
+            Assert.Throws<InvalidOperationException>(() => CreateRuleEngine(ruleSet));
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("MA")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeExpression[] { new CodePrimitiveExpression("AAA") }
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_constructor_invocation_missing_required_parameter_throws_InvalidOperationException()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+
+            //Act
+            Assert.Throws<InvalidOperationException>(() => CreateRuleEngine(ruleSet));
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("MA")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeObjectCreateExpression
+                    (
+                        new CodeTypeReference("SampleFlow.OtherEntity"),
+                        new CodeExpression[]
+                        {
+                            new CodePrimitiveExpression("AAA")
+                        }
+                    )
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_method_invocation_with_parameter_type_mismatch_throws_InvalidOperationException()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+
+            //Act
+            Assert.Throws<InvalidOperationException>(() => CreateRuleEngine(ruleSet));
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("MA")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeExpression[]
+                    {
+                        new CodePrimitiveExpression(1),
+                        new CodePrimitiveExpression("BBB")
+                    }
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_constructor_invocation_with_parameter_type_mismatch_throws_InvalidOperationException()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+
+            //Act
+            Assert.Throws<InvalidOperationException>(() => CreateRuleEngine(ruleSet));
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("MA")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeObjectCreateExpression
+                    (
+                        new CodeTypeReference("SampleFlow.OtherEntity"),
+                        new CodeExpression[]
+                        {
+                            new CodePrimitiveExpression(1),
+                            new CodePrimitiveExpression("BBB")
+                        }
+                    )
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_method_invocation_with_too_many_parameters_throws_InvalidOperationException()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+
+            //Act
+            Assert.Throws<InvalidOperationException>(() => CreateRuleEngine(ruleSet));
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("MA")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetMoreValues",
+                    new CodeExpression[]
+                    {
+                        new CodePrimitiveExpression("AAA"),
+                        new CodePrimitiveExpression("BBB"),
+                        new CodePrimitiveExpression("CCC")
+                    }
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_constructor_invocation_with_too_many_parameters_throws_InvalidOperationException()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+
+            //Act
+            Assert.Throws<InvalidOperationException>(() => CreateRuleEngine(ruleSet));
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("MA")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeObjectCreateExpression
+                    (
+                        new CodeTypeReference("SampleFlow.YetAnotherEntity"),
+                        new CodeExpression[]
+                        {
+                            new CodePrimitiveExpression("AAA"),
+                            new CodePrimitiveExpression("BBB"),
+                            new CodePrimitiveExpression("CCC")
+                        }
+                    )
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_method_invocation_with_missing_optional_parameters_succeeds()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+            RuleEngine ruleEngine = CreateRuleEngine(ruleSet);
+            SampleFlow.FlowEntity entity = new SampleFlow.FlowEntity
+            {
+                State = "NC"
+            };
+
+            //Act
+            ruleEngine.Execute(entity);
+
+            //Assert
+            Assert.Equal("AAA", entity.FirstValue);
+            Assert.Equal("BBB", entity.SecondValue);
+            Assert.Equal("", entity.ThirdValue);
+            Assert.Equal(0, entity.FourthValue);
+            Assert.Null(entity.TheParams);
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("NC")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeExpression[]
+                    {
+                        new CodePrimitiveExpression("AAA"),
+                        new CodePrimitiveExpression("BBB")
+                    }
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_constructor_invocation_with_missing_optional_parameters_succeeds()
+        {
+            //Arrange
+            RuleSet ruleSet = new RuleSet { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+            RuleEngine ruleEngine = CreateRuleEngine(ruleSet);
+            SampleFlow.FlowEntity entity = new SampleFlow.FlowEntity
+            {
+                State = "NC"
+            };
+
+            //Act
+            ruleEngine.Execute(entity);
+
+            //Assert
+            Assert.Equal("AAA", entity.FirstValue);
+            Assert.Equal("BBB", entity.SecondValue);
+            Assert.Equal("", entity.ThirdValue);
+            Assert.Equal(0, entity.FourthValue);
+            Assert.Null(entity.TheParams);
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new CodeBinaryOperatorExpression
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("NC")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new CodeMethodInvokeExpression
+                (
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeObjectCreateExpression
+                    (
+                        new CodeTypeReference("SampleFlow.OtherEntity"),
+                        new CodeExpression[]
+                        {
+                            new CodePrimitiveExpression("AAA"),
+                            new CodePrimitiveExpression("BBB")
+                        }
+                    )
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_method_invocation_with_missing_optional_parameters_succeeds_without_params_argument()
+        {
+            //Arrange
+            RuleSet ruleSet = new() { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+            RuleEngine ruleEngine = CreateRuleEngine(ruleSet);
+            SampleFlow.FlowEntity entity = new()
+            {
+                State = "NC"
+            };
+
+            //Act
+            ruleEngine.Execute(entity);
+
+            //Assert
+            Assert.Equal("AAA", entity.FirstValue);
+            Assert.Equal("BBB", entity.SecondValue);
+            Assert.Equal("", entity.ThirdValue);
+            Assert.Equal(0, entity.FourthValue);
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new()
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("NC")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new                (
+                    new CodeThisReferenceExpression(),
+                    "SetValuesWithoutParams",
+                    [
+                        new CodePrimitiveExpression("AAA"),
+                        new CodePrimitiveExpression("BBB")
+                    ]
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        [Fact]
+        public void Test_Rule_constructor_invocation_with_missing_optional_parameters_succeeds_without_params_argument()
+        {
+            //Arrange
+            RuleSet ruleSet = new() { Name = "MyRuleSet", ChainingBehavior = RuleChainingBehavior.Full };
+            ruleSet.Rules.Add(BuildRule());
+            RuleEngine ruleEngine = CreateRuleEngine(ruleSet);
+            SampleFlow.FlowEntity entity = new()
+            {
+                State = "NC"
+            };
+
+            //Act
+            ruleEngine.Execute(entity);
+
+            //Assert
+            Assert.Equal("AAA", entity.FirstValue);
+            Assert.Equal("BBB", entity.SecondValue);
+            Assert.Equal("", entity.ThirdValue);
+            Assert.Equal(0, entity.FourthValue);
+
+            Rule BuildRule()
+            {
+                // define first predicate: this.State == "MA"
+                CodeBinaryOperatorExpression ruleStateTest = new()
+                {
+                    Left = new CodePropertyReferenceExpression(new CodeThisReferenceExpression(), "State"),
+                    Operator = CodeBinaryOperatorType.ValueEquality,
+                    Right = new CodePrimitiveExpression("NC")
+                };
+
+                //action SampleFlow.FlowEntity.SetDefaultState("NH")
+                CodeMethodInvokeExpression methodInvoke = new(
+                    new CodeThisReferenceExpression(),
+                    "SetValues",
+                    new CodeObjectCreateExpression
+                    (
+                        new CodeTypeReference("SampleFlow.EntityWithoutParams"),
+                        [
+                            new CodePrimitiveExpression("AAA"),
+                            new CodePrimitiveExpression("BBB")
+                        ]
+                    )
+                );
+
+                return new Rule("Rule1")
+                {
+                    Condition = new RuleExpressionCondition(ruleStateTest),
+                    ThenActions = { new RuleStatementAction(methodInvoke) }
+                };
+            }
+        }
+
+        private RuleEngine CreateRuleEngine(RuleSet ruleSet)
+        {
+            return new RuleEngine(ruleSet, Helper.GetValidation(ruleSet, typeof(SampleFlow.FlowEntity)));
+        }
+    }
+}
