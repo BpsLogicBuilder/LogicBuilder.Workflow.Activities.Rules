ccb7f358c5cf48d273e00709cac56b31900eea89
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-02-05 11:45:59 -0500

    Add project files.

---
+++ LogicBuilder.Workflow.Activities.Rules/Common/AssemblyRef.cs
@@ -0,0 +1,14 @@
+﻿/*********************************************************************
+ * NOTE: A copy of this file exists at: WF\Common\Shared
+ * The two files must be kept in sync.  Any change made here must also
+ * be made to WF\Common\Shared\AssemblyRef.cs
+*********************************************************************/
+namespace LogicBuilder.Workflow.Activities.Common
+{
+    public class AssemblyRef
+    {
+        public const string ActivitiesAssemblyRef = "LogicBuilder.Workflow.Activities.Rules, Version=2.0.0.0, Culture=neutral, PublicKeyToken=646893bec0268535";
+        public const string DialogAssemblyRef = "LogicBuilder.Workflow.Activities.Rules.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=646893bec0268535";
+        public const string SystemCore = "System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e";
+    }
+}
